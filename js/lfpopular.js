44cdca2e7807dce157fd9cd13f8f7745bcb2e1a8
Sonyl Nagale <sonyl@livefyre.com>
2013-11-20 22:27:29 -0500

    For next client review

--- js/lfpopular.js
+++ js/lfpopular.js
@@ -4,97 +4,69 @@ var LF = LF || {};
 	
 /**
  * lfpopular
- * Takes an object of options, finds the highest heat from that,
- * and sets a DOM element's contents to that content
+ * Finds the given number of Featured items from the given collections
  * @author Sonyl Nagale <sonyl@livefyre.com>
- * @version 0.1
+ * @version 0.3
  * @param {Object} opts = {
  * 		el: String (required)
- * 		tag: String 
- * 		siteId: String
- * 		network: String (required)
- * 		truncate: String
+ * 		collections: Array (required) [ name (String): {
+					'network': String,
+					'siteId': String,
+					'articleId':String
+				}
+		]
+ * 		items: String
  * }
  * @returns {LF.lfpopular} this instance
  */
 LF.lfpopular = function(opts) {
+	var $ = Livefyre.require('streamhub-sdk/jquery');
 	var defaults = {
 		'el':null,
-		'tag': null,
-		'siteId': null,
-		'network': null,
-		'truncate': '40'
+		'collections': null,
+		'items': 3
 	};
 	
 	this.opts = opts || {};
 	this.opts = $.extend(defaults, this.opts);
 
 	// error handling
-	if (this.opts.el == null || this.opts.network == null || (this.opts.tag == null && this.opts.siteId == null)) {
+	if (this.opts.el == null || this.opts.collections == null) {
 		throw "Not enough parameters defined";
 		return;
 	}
 	
 	this.$el = $(this.opts.el);
 	
-	this._getHeat();
+	this._getFeatured();
 };
 
 /**
  * @private
- * Constructs URL to fetch heat from Heat API
+ * Creates collections from opts and gets their featured items 
  */
-LF.lfpopular.prototype._getHeat = function() {
-	var delimeter = "?";
-	
-	var url = "http://bootstrap." + this.opts.network + "/api/v3.0/hottest/";
-	
-	if (this.opts.tag != null) {
-		url += delimeter + "tag=" + this.opts.tag;
-		delimeter = "&";
+LF.lfpopular.prototype._getFeatured = function() {
+	var collections = [];
+	var Collection = Livefyre.require('streamhub-sdk/collection')
+
+	for (var i = 0; i < this.opts.collections.length; ++i) {
+		var collection = this.opts.collections[i];
+		
+		collections.push(new Collection({
+			"network":collection.network,
+			"siteId":collection.siteId,
+			"articleId":collection.articleId
+		}).createFeaturedContents().createArchive());
 	}
 	
-	if (this.opts.site != null) {
-		url += delimeter + "site=" + this.opts.site;
-		delimeter = "&";
+	for (var i = 0; i< collections.length; ++i) {
+		collections[i].on('data', $.proxy(function(e) {
+			this._setContent(e);
+		},this));
 	}
 	
-	if (this.opts.number != null) {
-		url += delimeter + "number=" + this.opts.number;
-	}
-	
-	$.ajax({
-	    url:url,
-	    dataType: 'jsonp', 
-	    success:$.proxy(function(json){
-	        this._getContent(json);
-	    },this),
-	    error:function(){
-	        // shh
-	    }
-	});		
 };
 
-/**
- * @private
- * Called by ajax return method from _getHeat
- * @param {Object} data
- */
-LF.lfpopular.prototype._getContent = function(data) {
-	var url = "http://bootstrap." + this.opts.network;
-	url += data.data[0].initUrl;
-	
-	$.ajax({
-	    url:url,
-	    dataType: 'jsonp', 
-	    success:$.proxy(function(json){
-	        this._setContent(json);
-	    },this),
-	    error:function(){
-	        // shh
-	    }
-	});		
-};
 
 /**
  * @private
@@ -102,115 +74,43 @@ LF.lfpopular.prototype._getContent = function(data) {
  * Actually sets DOM info
  * @param {Object} data
  */
-LF.lfpopular.prototype._setContent = function(data) {
-	this.$el.html(this._truncateTitle(data.headDocument.content[0].content.bodyHtml));
-	
-	this.$retweet = $('#lf-retweetAction');
-
-	this.$retweet.click(function(e) {
-		var url = "https://twitter.com/intent/retweet?tweet_id=";
-		var tweetId = data.headDocument.content[0].content.id.replace('tweet-','');
-		tweetId = tweetId.replace('\@twitter.com','');
-		window.open(url + tweetId, 'new',"width=500,height=300,left=" + (screen.width/2) + ",top=" + (screen.height/2));
+LF.lfpopular.prototype._setContent = function(e) {
+	var ListView = Livefyre.require('streamhub-sdk/content/views/content-list-view');
+	var ContentView = Livefyre.require('streamhub-sdk/content/views/content-view');
+	var Content = Livefyre.require('streamhub-sdk/content');	
+					
+	var div = $('<p/>', {
+		id: 'tweet' + e.tweetId,
+		html: '<p>' + e.body + '</p><p><a class="lf-retweet-action" id="retweet-' + e.tweetId + '">Retweet</a></p>'
 	});
+	
+	this.$el.append(div);
+	
+	$(div).click(function(e) {
+		console.log(e.target);
+		var url = "https://twitter.com/intent/retweet?tweet_id=" + e.target.id.replace('retweet-','');
+		window.open(url, 'new',"width=500,height=300,left=" + (screen.width/2) + ",top=" + (screen.height/2));
+
+	});
+
+	var content = new Content($(div).html());
+
+	
+	var contentview = new ContentView({
+		content: content, 
+		el: document.getElementById('tweet' + e.tweetId)
+	}).render();
+	
+	if (typeof this.timer == 'undefined') {
+		this.timer = setInterval($.proxy(function() {
+			var item = this.$el.children()[0];
+			$(item).fadeOut($.proxy(function() {
+				var newitem = $(item).detach().show();
+				this.$el.append(newitem);
+			},this));
+		},this), 5000);
+	}
 };
-
-/**
- * @private
- * Truncates data on wordbreaks
- * @todo Do we have an internal one?
- * @see https://github.com/pathable/truncate
- * @param data
- * @return {String} truncated data
- */
-
-LF.lfpopular.prototype._truncateTitle = function(data) {
-	return $.truncate(data, { length: this.opts.truncate });
-};
-
-/**
- * Truncation helper
- * @see https://github.com/pathable/truncate/blob/master/jquery.truncate.js
- */
-var chop = /(\s*\S+|\s)$/;
-
-// Return a truncated html string.  Delegates to $.fn.truncate.
-$.truncate = function(html, options) {
-  return $('<div></div>').append(html).truncate(options).html();
-};
-
-// Truncate the contents of an element in place.
-$.fn.truncate = function(options) {
-  if ($.isNumeric(options)) options = {length: options};
-  var o = $.extend({}, $.truncate.defaults, options);
-
-  return this.each(function() {
-    var self = $(this);
-
-    if (o.noBreaks) self.find('br').replaceWith(' ');
-
-    var text = self.text();
-    var excess = text.length - o.length;
-
-    if (o.stripTags) self.text(text);
-
-    // Chop off any partial words if appropriate.
-    if (o.words && excess > 0) {
-      excess = text.length - text.slice(0, o.length).replace(chop, '').length - 1;
-    }
-
-    if (excess < 0 || !excess && !o.truncated) return;
-
-    // Iterate over each child node in reverse, removing excess text.
-    $.each(self.contents().get().reverse(), function(i, el) {
-      var $el = $(el);
-      var text = $el.text();
-      var length = text.length;
-
-      // If the text is longer than the excess, remove the node and continue.
-      if (length <= excess) {
-        o.truncated = true;
-        excess -= length;
-        $el.remove();
-        return;
-      }
-
-      // Remove the excess text and append the ellipsis.
-      if (el.nodeType === 3) {
-        $(el.splitText(length - excess - 1)).replaceWith(o.ellipsis);
-        return false;
-      }
-
-      // Recursively truncate child nodes.
-      $el.truncate($.extend(o, {length: length - excess}));
-      return false;
-    });
-  });
-};
-
-$.truncate.defaults = {
-
-  // Strip all html elements, leaving only plain text.
-  stripTags: false,
-
-  // Only truncate at word boundaries.
-  words: true,
-
-  // Replace instances of <br> with a single space.
-  noBreaks: false,
-
-  // The maximum length of the truncated html.
-  length: Infinity,
-
-  // The character to use as the ellipsis.  The word joiner (U+2060) can be
-  // used to prevent a hanging ellipsis, but displays incorrectly in Chrome
-  // on Windows 7.
-  // http://code.google.com/p/chromium/issues/detail?id=68323
-  ellipsis: ' \u2026' // '\u2060\u2026'
-
-};
-
-
 
 
 })(Livefyre.require('streamhub-sdk/jquery'));
