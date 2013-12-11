375eb8f4e4bd68ce48a8227015dcc17d35e5ecf9
sonylnagale <sonyl@livefyre.com>
2013-12-10 22:40:42 -0500

    Progress with example

--- cmg/three-column-social-hub/js/lfsocialhub.js
+++ cmg/three-column-social-hub/js/lfsocialhub.js
@@ -1,5 +1,12 @@
 var LF = LF || {};
 
+var doShare = function(data) {
+	var $ = Livefyre.require('streamhub-sdk/jquery');
+	
+	janrain.engage.share.setUrl($(data).data('content-action-share-link'));
+	janrain.engage.share.show();
+};
+
 
 (function() {
 
@@ -122,38 +129,120 @@ LF.lfsocialhub = function(opts) {
 		}
 	},this));
 };
-	
+
 /**
  * @private
  * Prepares the collections and views
  */
 LF.lfsocialhub.prototype._prepData = function() {
-	var ListView = Livefyre.require('streamhub-sdk/content/views/content-list-view');
-	var Collection = Livefyre.require('streamhub-sdk/collection');
-	
-	
-	for (var i = 0; i < this.opts.collections.length; ++i) {
-		var collection = this.opts.collections[i];
+//	var ListView = Livefyre.require('streamhub-sdk/content/views/content-list-view');
+//	var Collection = Livefyre.require('streamhub-sdk/collection');
+//	var ContentView = Livefyre.require('streamhub-sdk/content/views/content-view');
+//	var Collection = Livefyre.require('streamhub-sdk/collection');
+//	var Content = Livefyre.require('streamhub-sdk/content');
+//	var LivefyreContent = Livefyre.require('')
+	Livefyre.require([
+            'streamhub-sdk/content/views/content-list-view',
+            'streamhub-sdk/content/views/content-view',
+            'streamhub-sdk/collection',
+            'streamhub-sdk/content',
+            'streamhub-sdk/content/types/livefyre-content',
+            'inherits',
+            'hogan'
+        ],$.proxy(function (ListView, ContentView, Collection, Content, LivefyreContent,
+            inherits, hogan) {
 		
-		this.links.push(collection.name); // for the headers
-		
-		this.collections[collection.name + "Collection"] = new Collection({
-			network: collection.network,
-			siteId: collection.siteId,
-			articleId: collection.articleId
-		});
+			for (var i = 0; i < this.opts.collections.length; ++i) {
+				var collection = this.opts.collections[i];
 				
-		if (!this.isHandheld) { // don't do this for handheld since we're going to do a 1-column view
-			this.views[collection.name + "View"] = new ListView({
-				initial: 50,
-				el: $('#' + collection.name + "Feed")
-			});
+				this.links.push(collection.name); // for the headers
+				
+				this.collections[collection.name + "Collection"] = new Collection({
+					network: collection.network,
+					siteId: collection.siteId,
+					articleId: collection.articleId
+				});
+						
+				if (!this.isHandheld) { // don't do this for handheld since we're going to do a 1-column view
+					this.views[collection.name + "View"] = new ListView({
+						initial: 50,
+						el: $('#' + collection.name + "Feed")
+					});					
+					
+		            HasCustomRssContentView.call(this.views[collection.name + "View"]);
+
+		            
+		            
+		            
+					this.collections[collection.name + "Collection"].pipe(this.views[collection.name + "View"]);
+				}
+			}
 			
-			this.collections[collection.name + "Collection"].pipe(this.views[collection.name + "View"]);
-		}
-	}
-	
-	this._setEvents();
+			this._setEvents();
+			
+			
+			
+			
+			/**
+             * A Custom RSS ContentView
+             */
+            function CustomRssContentView (opts) {
+                ContentView.apply(this, arguments);
+            }
+            inherits(CustomRssContentView, ContentView);
+
+            CustomRssContentView.prototype.elClass += ' custom-rss-content-view';
+
+            /**
+             * It has a custom template, which we've stored in a script element
+             * above
+             */
+            var mustacheTemplate = this._rssMustache(),
+                compiledTemplate = hogan.compile(mustacheTemplate);
+            CustomRssContentView.prototype.template = function (context) {
+                // remove this later, but here you can see what
+                // variables you can use in your template
+                // meta.content.feedEntry will contain some data
+                // from the RSS Feed
+                //console.log("Rendering template for custom ContentView", context);
+
+                return compiledTemplate.render(context);
+            };
+            
+
+            /**
+             * Mixin to a ContentListView such that it will render a
+             * CustomRssContentView for RSS Content
+             */
+            function HasCustomRssContentView () {
+                /**
+                 * Override ListView#createContentView to create a special ContentView
+                 * class for RSS Items
+                 */
+                var ogCreateContentView = this.createContentView;
+                this.createContentView = function (content) {
+                    if (content.source === 'feed') {
+                        return makeCustomContentView(content);
+                    }
+                    return ogCreateContentView.apply(this, arguments);
+                }
+
+                /**
+                 * Create a rendered custom ContentView for the provided content
+                 */
+                function makeCustomContentView (content) {
+                    var contentView = new CustomRssContentView({
+                        content: content
+                    });
+                    contentView.render();
+                    return contentView;
+                }
+            }
+            
+            
+            
+            
+	},this));
 };
 
 /**
@@ -232,6 +321,10 @@ LF.lfsocialhub.prototype._setWall = function() {
 				
 		this.desiredCollection.pipe(this.wallView);
 	},this));
+};
+
+LF.lfsocialhub.prototype._rssMustache = function() {
+	return '<div class="content-header"><div class="content-header-inner">{{#author.avatar}}<a class="content-author-avatar"><img src="{{author.avatar}}"/></a>{{/author.avatar}}<div class="content-byline">{{#author.url}}<a class="content-author-name" href="{{author.url}}" target="_blank">{{author.displayName}}</a>{{/author.url}}{{^author.url}}{{#author.profileUrl}}<a class="content-author-name" href="{{author.profileUrl}}" target="_blank">{{author.displayName}}</a>{{/author.profileUrl}}{{^author.profileUrl}}<span class="content-author-name">{{author.displayName}}</span>{{/author.profileUrl}}{{/author.url}}</div></div></div><div class="content-attachments"></div><div class="content-body">{{{body}}}</div>{{#featured}}<div class="content-featured">Featured</div>{{/featured}}<ul class="content-actions"><li class="content-action" data-content-action="share"><a class="hub-tooltip-link content-action-share" data-content-action-share-link="{{meta.content.feedEntry.link}}" onClick="doShare(this)" title="Share"><span>Share</span></a></li></ul><div class="content-meta">{{#formattedCreatedAt}}<div class="content-created-at">{{{formattedCreatedAt}}}</div>{{/formattedCreatedAt}}</div>';
 };
 
 })();
