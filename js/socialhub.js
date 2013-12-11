6f5f53a87f4cdb450bc3d81d3822494e52fbda48
sonylnagale <sonyl@livefyre.com>
2013-12-11 10:44:27 -0500

    Bundler for deliery.

---
+++ js/socialhub.js
@@ -0,0 +1,15 @@
+<?php
+    header("Content-type: text/javascript");
+	
+	$files = array(
+		'lfsocialhub.js',
+		'lfpopular.js',
+	);
+	
+	$return = '';
+	
+	foreach ($files as $file) {
+		$return .= file_get_contents($file);
+	}
+	
+	echo $return;
