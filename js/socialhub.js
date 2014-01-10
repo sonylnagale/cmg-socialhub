a34fd61a1666bb089125a51a7032b530cf196aa9
sonylnagale <sonyl@livefyre.com>
2014-01-09 21:21:31 -0500

    BG image for blue bar for < IE10

--- js/socialhub.js
+++ js/socialhub.js
@@ -1,7 +1,7 @@
 <?php
     header("Content-type: text/javascript");
 	
-	$version = '1.1.2';
+	$version = '1.1.3';
 	$build_date = date('c');
 
 	$files = array(
@@ -22,6 +22,7 @@
 	
 	RELEASE NOTES
 	
+	v1.1.3: Sync
 	v1.1.2: Sync
 	v1.1.1: Allow Facebook replies
 	
