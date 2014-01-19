a6abad9d332bfb660de89e4c74d1e5d94d3a6298
sonylnagale <sonyl@livefyre.com>
2014-01-19 09:34:25 -0500

    Increment version

--- cmg/three-column-social-hub/js/socialhub.js
+++ cmg/three-column-social-hub/js/socialhub.js
@@ -1,7 +1,7 @@
 <?php
     header("Content-type: text/javascript");
 	
-	$version = '1.1.3';
+	$version = '1.1.3.2';
 	$build_date = date('c');
 
 	$files = array(
@@ -21,7 +21,7 @@
 	$releasenotes = <<<EOF
 /**	
 	RELEASE NOTES
-	
+	v1.1.3.2: Hotfix for Instagram avatars onerror
 	v1.1.3: Sync
 	v1.1.2: Sync
 	v1.1.1: Allow Facebook replies
